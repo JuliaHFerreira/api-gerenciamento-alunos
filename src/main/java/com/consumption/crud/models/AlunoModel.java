a8738ab61c4e0ab7baa8f8e22fdd1dd0c8359b4d
JuliaHFerreira <juliahelena317@gmail.com>
2025-02-20 21:35:40 -0300

    Iniciando projeto - dependencias maven instaladas, configurado application e classe modelo

---
+++ src/main/java/com/consumption/crud/models/AlunoModel.java
@@ -0,0 +1,10 @@
+package com.consumption.crud.models;
+
+public class AlunoModel {
+    private int id;
+    private String nomeAluno;
+    private String sobrenomeAluno;
+    private String email;
+    private String cpf;
+    private String rg;
+}
